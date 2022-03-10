0e335b9945d195b66065006f2c7d0809cb86680d
madeinkgb@gmail.com <madeinkgb@gmail.com>
2022-03-10 16:58:29 +0900

    commit

--- TreasureIsland/src/main/java/com/happy/world/usage/basic/StringUsage.java
+++ TreasureIsland/src/main/java/com/happy/world/usage/basic/StringUsage.java
@@ -40,11 +40,28 @@ public class StringUsage {
 		Out.print(s3.toString());
 	}
 	
+	
+	public static void replace() {
+		String str1 = "#{이름}님 안녕하세요. #{이름}님께 드리는 이벤트";
+		String str2 = "홍길동";
+		Out.print(str1.replace("#{이름}", str2));
+		//Out.print(str1.replaceAll("#{이름}", str2));
+		
+	}
+	
+	public static void withs() {
+		String str1 = "abcde/";
+		if(str1.endsWith("/"))Out.print("withs");
+	}
 	public static void main(String[] args) {
 		StringUsage.substring();
 		//StringUsage.throwOutBoundException();
 		Out.cutLine();
 		StringUsage.typeToString();
+		Out.cutLine();
+		StringUsage.replace();
+		Out.cutLine();
+		StringUsage.withs();
 	}
 }
 
